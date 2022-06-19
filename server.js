9f01dbe29944cdb4be394ec006d70c15e5bf9f08
Adejumo <excellencejumo@gmail.com>
2022-06-19 15:15:14 +0100

    update

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const http = require("http");
-const { url } = require("inspector");
+const fs = require("fs")
 http.createServer((req,res)=>{
     switch(req.url){
-        case '/':
+        case '/home':
             res.writeHead(200,{'Content-Type':'text/html'})
-            res.end('home.html')
+            fs.createReadStream('home.html').pipe(res);
             break;
-        
+        case '/about':
+            res.writeHead(200,{'Content-Type':'text/html'})
+            fs.createReadStream('about.html').pipe(res);
+            break;
+        case '/contact':
+            res.writeHead(200,{'Content-Type':'text/html'})
+            fs.createReadStream('contact.html').pipe(res);
+            break;
+        default:
+            res.writeHead(200,{'Content-Type':'text/html'})
+            fs.createReadStream('home.html').pipe(res);
+            break;
         }
 }).listen(8000,()=>{
     console.log("Hello World")
