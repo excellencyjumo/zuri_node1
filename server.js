70bce3aaf4162e5d28ee0e2d2c5df9eb389901bd
Adejumo <excellencejumo@gmail.com>
2022-06-19 14:50:40 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 const { url } = require("inspector");
 http.createServer((req,res)=>{
-    
+    switch(req.url){
+        case '/':
+            res.writeHead(200,{'Content-Type':'text/html'})
+            break;
+        
+        }
 }).listen(8000,()=>{
     console.log("Hello World")
 })
