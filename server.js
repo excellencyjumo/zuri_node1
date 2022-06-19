6d03a8b3d0f7c62027074d49e5343a8cd8f431ed
Adejumo <excellencejumo@gmail.com>
2022-06-19 14:44:05 +0100

    ...

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 const http = require("http");
 const { url } = require("inspector");
-const app=http.createServer();
-http.createServer("/home",(req,res)=>{
-    switch(req.url){
-        case "/home":
-            res.send("HOME PAGE")
-            break;
-        case "/about":
-            res.send("ABOUT PAGE")
-            break;
-        case "/contact":
-            res.send("ANOTHER PAGE")
-            break;    
-        }
-})
-app.listen(8000,()=>{
+http.createServer((req,res)=>{
+    
+}).listen(8000,()=>{
     console.log("Hello World")
 })
